a97a78e8c9c1520eccf655beebf18066a6ed2dff
zm00622 <zarymanning@gmail.com>
2020-02-06 15:07:15 -0500

    User Generated Stock Input
    This code adds the ability for the user to change the stock depicted on the chart based off of their input into the input box.
    
    The two commits before this are pretty aweful attempts. I was making the simple mistake of not binding this to fetch stock in the return/onClick event. The broswer was expecting a function but instead receiving a string. Lesson learned. I know what that error message means now.

--- src/Stock.js
+++ src/Stock.js
@@ -9,120 +9,98 @@ class Stock extends React.Component {
     this.state = {
       stockChartXValues: [],
       stockChartYValues: [],
-      // InputBoxValue: '',
     }
   }
 
-  componentDidMount() {
-    this.fetchStock();
-  }
+ fetchStock() {
 
+     const pointerToThis = this;
+     console.log(pointerToThis);
+     const API_KEY = process.env.API_KEY;
+     let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${document.getElementById("ticker").value}&outputsize=compact&apikey=${API_KEY}`;
+     let stockChartXValuesFunction = [];
+     let stockChartYValuesFunction = [];
 
-  fetchStock() {
+     fetch(API_Call)
+       .then(
+         function(response) {
+           return response.json();
+         }
+       )
+       .then(
+         function(data) {
+           console.log(data);
 
-    const pointerToThis = this;
-    console.log(pointerToThis);
-    const API_KEY = process.env.API_KEY;
-    let StockSymbol = 'TSLA';
-    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
-    let stockChartXValuesFunction = [];
-    let stockChartYValuesFunction = [];
+           for (var key in data['Time Series (Daily)']) {
+             stockChartXValuesFunction.push(key);
+             stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+           }
 
-    fetch(API_Call)
-      .then(
-        function(response) {
-          return response.json();
-        }
-      )
-      .then(
-        function(data) {
-          console.log(data);
+           // console.log(stockChartXValuesFunction);
+           pointerToThis.setState({
+             stockChartXValues: stockChartXValuesFunction,
+             stockChartYValues: stockChartYValuesFunction
+           });
+         } )
+   }
 
-          for (var key in data['Time Series (Daily)']) {
-            stockChartXValuesFunction.push(key);
-            stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
-          }
+   render() {
 
-          // console.log(stockChartXValuesFunction);
-          pointerToThis.setState({
-            stockChartXValues: stockChartXValuesFunction,
-            stockChartYValues: stockChartYValuesFunction
-          });
-        } )
-  }
+     return (
+       <div>
+         <h1>Zarys React Stock Market App</h1>
+           <h1>Enter your Ticker</h1>
+           <input id="ticker" type="text"></input>
+           <button id="submit" onClick={this.fetchStock.bind(this)}>Submit</button>
+           <h3 id="DisplayText"></h3>
+         <Plot
+           data={[
+             {
+               x: this.state.stockChartXValues,
+               y: this.state.stockChartYValues,
+               type: 'scatter',
+               // fill: 'tozeroy',
+               mode: 'lines+markers',
+               marker: {
+                 color: '#08cf98',
+                 symbol: "cross-thin"
+               },
+             }
+           ]}
+           layout={
+             {
+               width: 720,
+               height: 440,
+               title: '100 Day Chart for Ticker "TSLA"',
+               paper_bgcolor: "#F2F2F2",
+               plot_bgcolor: "#36474f",
+               xaxis: {
+                 color: "#08cf98",
+               },
+               yaxis: {
+                 color: "#08cf98"
+               },
+               margin: {
+                 pad: 20,
+               },
+               font: {
+                 color: "#08cf98",
+               },
+             }
+           }
+         />
+       </div>
+     )
+   }
+ }
 
-  render() {
-
-    function handleSubmit (e) {
-      e.preventDefault()
-      var InputBoxValue = document.getElementById("InputBox").value;
-      document.getElementById("DisplayText").innerHTML=InputBoxValue;
-
-    //  Add code for inproper input error here
-    }
-
-    return (
-      <div>
-        <h1>React Stock Market App</h1>
-        <form onSubmit={handleSubmit}>
-          <h1>Enter your Ticker</h1>
-          <input id="InputBox" type="text"></input>
-          <button>Submit</button>
-          <h3 id="DisplayText"></h3>
-        </form>
-        <Plot
-          data={[
-            {
-              x: this.state.stockChartXValues,
-              y: this.state.stockChartYValues,
-              type: 'scatter',
-              // fill: 'tozeroy',
-              mode: 'lines+markers',
-              marker: {
-                color: '#08cf98',
-                symbol: "cross-thin"
-              },
-            }
-          ]}
-          layout={
-            {
-              width: 720,
-              height: 440,
-              title: '100 Day Chart for Ticker "TSLA"',
-              paper_bgcolor: "#F2F2F2",
-              plot_bgcolor: "#36474f",
-              xaxis: {
-                color: "#08cf98",
-              },
-              yaxis: {
-                color: "#08cf98"
-              },
-              margin: {
-                pad: 20,
-              },
-              font: {
-                color: "#08cf98",
-              },
-            }
-          }
-        />
-      </div>
-    )
-  }
-}
-
-export default Stock;
+ export default Stock;
 
 
 
 
 
-
-
-
-
-
-
+// Attempt at changing modeBarButtons to add functionality and different charts
 // DEFAULT_CHART_CONFIG = { {modeBarButtons: [ 'sendDataToCloud' ] } }
 
 // config = {
